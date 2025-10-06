58932546d734a99971e108568218700969fd3627
caiostato <zombieiron9@gmail.com>
2025-10-06 17:21:39 -0300

    fix: Adjust footer content

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -3,7 +3,7 @@ const Footer = () => {
     <footer className="border-t border-primary/10  px-4 py-8 bg-[#002a53]">
       <div className="mx-auto max-w-6xl text-center">
         <p className="text-sm text-white/70">
-          © {new Date().getFullYear()} Your Name. All rights reserved.
+          © {new Date().getFullYear()} Cerasos. All rights reserved.
         </p>
         <p className="mt-2 text-sm text-primary">
           Built with passion and dedication
