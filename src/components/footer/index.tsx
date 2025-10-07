f4eb2fa5be39a1b1b875ddc5d2145fa4f750da4c
caiostato <zombieiron9@gmail.com>
2025-10-07 13:18:35 -0300

    fix: Adjust footer content

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -3,10 +3,7 @@ const Footer = () => {
     <footer className="border-t border-primary/10  px-4 py-8 bg-[#002a53]">
       <div className="mx-auto max-w-6xl text-center">
         <p className="text-sm text-white/70">
-          © {new Date().getFullYear()} Cerasos. All rights reserved.
-        </p>
-        <p className="mt-2 text-sm text-primary">
-          Built with passion and dedication
+          © {new Date().getFullYear()} Cerasos. Todos direitos reservados.
         </p>
       </div>
     </footer>
